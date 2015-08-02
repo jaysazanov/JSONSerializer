fc6fd6db09ac316a88f69110fee1b24f29f21970
jaysazanov <jaysazanov@outlook.com>
2015-08-03 02:46:25 +0300

    Full

---
+++ BubbleSorter/BubbleSorter/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSorter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee[] employees =
+            {
+                new Employee("Bugs Bunny", 20000),
+                new Employee("Elmer Fudd", 10000),
+                new Employee("Duffy Duck" ,1000000.38m),
+                new Employee("Wile Coyote", 25000),
+                new Employee("RoadRunner", 50000)
+            };
+            BubbleSorter.Sort(employees, Employee.CompareSalary);
+            foreach(var employee in employees)
+            {
+                Console.WriteLine(employee);
+            }
+        }
+    }
+    class BubbleSorter
+    {
+        public static void Sort<T>(IList<T> sortArray, Func<T , T, bool> comparison)
+        {
+            bool swapped = true;
+            do
+            {
+                swapped = false;
+                for (int i = 0; i < sortArray.Count - 1; i++)
+                {
+                    if (comparison(sortArray[i + 1], sortArray[i]))
+                    {
+                        T temp = sortArray[i];
+                        sortArray[i] = sortArray[i + 1];
+                        sortArray[i + 1] = temp;
+                        swapped = true;
+                    }
+                }
+            } while (swapped);
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        int _a = 2;
+
+        static int b = 3;
+
+        private delegate string GetAString();
+
+        static void Main()
+        {
+            int s = 20;
+            GetAString firstStringMethod = new GetAString(s.ToString);
+            Console.WriteLine(firstStringMethod());
+            Console.WriteLine(firstStringMethod.Invoke());
+            sbyte o = 127;
+            //checked { ++o; }
+            ++o;
+            Console.WriteLine(o);
+            int r=9;
+            Console.WriteLine(R(ref r));
+            Console.WriteLine(DayOfWeek.Friday);
+            Console.WriteLine(r);
+            int z;
+            Console.WriteLine("z+a=out "+ F(out z));
+            const int n = 6;
+            int c = 4;
+            Program p = new Program();
+            Console.WriteLine("gET tYPE: "+p.GetType(), p.MemberwiseClone());
+            Console.WriteLine(p._a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(c.GetType());
+            //Console.WriteLine(F());
+            for(int i = 0; i < 3; i++)
+            {
+                int d = 5;
+                Console.WriteLine(d);
+            }
+            //Console.ReadLine();
+            return;
+        }
+        public static int R(ref int r)
+        {
+            r = 2;
+            return r;
+        }
+        public static int F(out int z)
+        {
+            z = 2;
+            int a = 5;
+            return z + a;
+        }
+    }
+}
